fc4037a2f04d611970ad1023335212a24132f148
SepCy <cyrillesepele3@gmail.com>
2019-12-15 14:45:57 +0100

    Intégration acf dans le template

--- public/themes/Emma/footer.php
+++ public/themes/Emma/footer.php
@@ -1,54 +1,91 @@
- <!-- Footer Area -->
+<?php
+$section_contact = get_field('section_contact');
+?>
 
- <div class="padding footer mt-md-4">
-     <footer class="mt-md-4">
-         <div class="container">
-             <div class="row">
-                 <div class="col-md-5 col-sm-7">
-                     <h3 class="text-md-left text-center mt-sm-4">Nos Contacts</h3><br>
-                     <ul class="contacts" style="float: left">
-                         <li class="contact-item"><i class="fas fa-phone-alt mr-3 pb-2"></i> +237 690214685</li>
-                         <li class="contact-item"><i class="fas fa-envelope mr-3 pb-2"></i>abc@xyz@gmail.com</li>
-                         <li class="contact-item"><i class="fab fa-whatsapp mr-3"></i>+237 690214685</li>
-                     </ul>
-                 </div>
-                 <div class="col-md-3 col-sm-5">
-                     <h3 class="text-center text-md-left mt-sm-4">Suivez-nous</h3><br>
-                     <div class="contacts" style="float: left ;display: flex; list-style-type: none; align-items:center;">
+<?php
+$section_follow = get_field('section_suivez_nous');
+?>
+<!-- Footer Area -->
 
-                         <li><i class="fab fa-facebook-f fa-2x ml-4 mr-4"></i></li>
-                         <li><i class="fab fa-youtube fa-2x"></i></li>
+<div class="padding footer mt-md-4">
+    <footer class="mt-md-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-5 col-sm-7">
+                    <h3 class="text-md-left text-center mt-sm-4">Nos Contacts</h3><br>
+                    <ul class="contacts" style="float: left">
+                        <li class="contact-item"><i class="fas fa-phone-alt mr-3 pb-2"></i>
+                            +237 <?php
+                                    echo $section_contact['numero'];
+                                    ?>
 
-                     </div>
-                 </div>
-                 <div class="col-md-4 col-sm-5">
-                     <h3 class="text-center text-md-right mt-sm-4">Horaires</h3><br>
-                     <div class="contacts" style="float: left ; list-style-type: none;">
+                        </li>
+                        <li class="contact-item"><i class="fas fa-envelope mr-3 pb-2"></i>
+                            <?php
+                            echo $section_contact['mail'];
+                            ?>
+                        </li>
+                        <li class="contact-item"><i class="fab fa-whatsapp mr-3"></i>
+                            +237 <?php
+                                    echo $section_contact['whatsapp'];
+                                    ?>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-3 col-sm-5">
+                    <h3 class="text-center text-md-left mt-sm-4">Suivez-nous</h3><br>
+                    <div class="contacts" style="float: left ;display: flex; list-style-type: none; align-items:center;">
 
-                         <li class="ml-4">Ouverts tous les jours : 24/24</li>
-                        
-                     </div>
-                 </div>
+                        <li> <a href="
+                        <?php
+                        echo $section_follow['lien_facebook'];
+                        ?>
+                        ">
+                                <i class="fab fa-facebook-f fa-2x ml-4 mr-4"></i>
+                        </li>
+                        </a>
+                        <li> <a href="
+                        <?php
+                        echo $section_follow['lien_youtube'];
+                        ?>
+                        ">
 
-             </div>
-         </div>
-     </footer>
- </div>
- </div>
- <!-- Script tags -->
+                                <i class="fab fa-youtube fa-2x">
 
- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
- <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
- <script>
-     AOS.init({
-         duration: 1200,
-         disable: 'mobile',
-         once: true
-     });
- </script>
- <?php wp_footer(); ?>
- </body>
+                                </i>
+                        </li>
+                        </a>
 
- </html>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-5">
+                    <h3 class="text-center text-md-right mt-sm-4">Horaires</h3><br>
+                    <div class="contacts" style="float: left ; list-style-type: none;">
+
+                        <li class="ml-4">Ouverts tous les jours : 24/24</li>
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </footer>
+</div>
+</div>
+<!-- Script tags -->
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<script>
+    AOS.init({
+        duration: 1200,
+        disable: 'mobile',
+        once: true
+    });
+</script>
+<?php wp_footer(); ?>
+</body>
+
+</html>
--- public/themes/Emma/front-page.php
+++ public/themes/Emma/front-page.php
@@ -1,15 +1,49 @@
 <?php
+$section_accueil = get_field('section_accueil');
+?>
+
+<?php
+$section_mariage = get_field('section_mariage');
+?>
+
+<?php
+$section_anniversaire = get_field('section_anniversaire');
+?>
+
+<?php
+$section_decoration = get_field('section_decoration');
+?>
+
+<?php
+$section_video = get_field('section_video');
+?>
+
+<?php
+$section_localisation = get_field('section_localisation');
+?>
+
+<?php
 
 get_header();
 ?>
+
+
 <div class="home">
     <div id="accueil">
         <div class="title" data-aos="zoom-in">
             <h3 class="welcome-text">Bienvenue chez mon <br>
                 Emma
             </h3>
-            <h5 class="slogan mt-3">L'art du beau et du bon</h5>
-            <a href="#" class="btn btn-outline-info mt-3">Consulter</a>
+            <h5 class="slogan mt-3">
+                <?php
+                echo $section_accueil['slogan'];
+                ?>
+            </h5>
+            <a href="
+            <?php
+            echo $section_accueil['lien'];
+            ?>
+                " class="btn btn-outline-info mt-3">Consulter</a>
         </div>
     </div>
 </div>
@@ -28,16 +62,22 @@ get_header();
         <div class="container">
             <div class="row">
                 <div class="col-9 col-lg-8 wedding-desc-block" data-aos="fade-right">
-                    <h3 class="wedding-title">Mariage</h3>
+                    <h3 class="wedding-title">
+                        <?php
+                        echo $section_mariage['titre'];
+                        ?>
+                    </h3>
                     <p class="wedding-description pt-md-4">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt cum accusamus, quasi sed dolor amet
-                        suscipit vitae maxime iste assumenda necessitatibus dolore eum voluptas neque reiciendis officiis
-                        voluptates debitis quaerat.
+                        <?php
+                        echo $section_mariage['message'];
+                        ?>
                     </p>
                     <a href="services.html" class="btn btn-outline-info mt-md-3">Voir plus</a>
                 </div>
                 <div class="col-3 col-lg-4" data-aos="fade-left">
-                    <img src="<?php bloginfo('template_directory') ?>/images/cake.png" alt="Gateau mariage" class="wedding-cake pt-sm-4">
+                    <img src="<?php
+                                echo esc_url($section_mariage['image']['url']);
+                                ?>" alt="Gateau mariage" class="wedding-cake pt-sm-4">
                 </div>
             </div>
         </div>
@@ -57,22 +97,30 @@ get_header();
         <div class="container">
             <div class="row" data-aos="fade-up-left">
                 <div class="col-12">
-                    <h3 class="birthday-title ">Anniversaire</h3>
+                    <h3 class="birthday-title ">
+                        <?php
+                        echo $section_anniversaire['titre'];
+                        ?>
+                    </h3>
                 </div>
             </div>
             <div class="row">
                 <div data-aos="fade-right" class="col-12 col-sm-6 col-md-4 mt-sm-4 birthday-cake-group">
-                    <img src="<?php bloginfo('template_directory') ?>/images/deva-williamson-HHebuDYjP4I-unsplash.jpg" alt="Image gateau d'anniversaire" class="birthday-cake-1 img-fluid">
-                    <img src="<?php bloginfo('template_directory') ?>/images/deva-williamson-HHebuDYjP4I-unsplash.jpg" alt="Image gateau d'anniversaire" class="birthday-cake-2 img-fluid
-                ">
+                    <img src="<?php
+                                echo esc_url($section_anniversaire['image_1']['url']);
+                                ?>" alt="Image gateau d'anniversaire" class="birthday-cake-1 img-fluid">
+
+                    <img src="<?php
+                                echo esc_url($section_anniversaire['image_2']['url']);
+                                ?>" class="birthday-cake-2 img-fluid">
                 </div>
                 <div class="col-sm-6 col-12 col-md-7 birthday-description ml-auto text-right" data-aos="fade-up-left">
                     <p class="pt-md-4">
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt optio doloribus dolore a dolorem quas animi
-                        ducimus qui, quo asperiores laboriosam odio deleniti minus error alias fugit doloremque distinctio
-                        molestiae!
+                        <?php
+                        echo $section_anniversaire['message'];
+                        ?>
                     </p>
-                    <a href="services.html" class="btn btn-outline-info mt-md-3" style="float: right">Voir plus</a>
+                    <a href="#" class="btn btn-outline-info mt-md-3" style="float: right">Voir plus</a>
                 </div>
             </div>
         </div>
@@ -89,7 +137,12 @@ get_header();
         <div class="container">
             <div class="row">
                 <div class="col-12">
-                    <h3 class="decoration-title">Décorations</h3>
+                    <h3 class="decoration-title">
+
+                        <?php
+                        echo $section_decoration['titre'];
+                        ?>
+                    </h3>
                     <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
                         <ol class="carousel-indicators">
                             <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
@@ -99,48 +152,76 @@ get_header();
                             <div class="carousel-item active">
                                 <div class="row">
                                     <div class="col-md-3 col-sm-4 mt-sm-4" data-aos-delay="300" data-aos="fade-up" data-aos-anchor-placement="top-bottom">
-                                        <img src="<?php bloginfo('template_directory') ?>/images/mitchell-lawler-tbaoryUol_E-unsplash.jpg" class="d-block w-100" alt="...">
-                                        <p class="deco-desc text-center center mt-2">Loreme ipqume</p>
+                                        <img src=" <?php
+                                                    $image1 = $section_decoration['images']['image_1'];
+                                                    echo esc_url($image1['image']['url']);
+                                                    ?>" class="d-block w-100" alt="...">
+                                        <p class="deco-desc text-center center mt-2"><?php echo $image1['description'] ?></p>
                                     </div>
                                     <div class="col-md-3 col-sm-4 mt-sm-4" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-delay="400">
-                                        <img src="<?php bloginfo('template_directory') ?>/images/mitchell-lawler-tbaoryUol_E-unsplash.jpg" class="d-block w-100" alt="...">
-                                        <p class="deco-desc text-center center mt-2">Loreme ipqume sjmkmlssoioi</p>
+                                        <img src="<?php
+                                                    $image2 = $section_decoration['images']['image_2'];
+                                                    echo esc_url($image2['image']['url']);
+                                                    ?>
+                                        " class="d-block w-100" alt="...">
+                                        <p class="deco-desc text-center center mt-2"><?php echo $image2['description'] ?></p>
                                     </div>
                                     <div class="col-md-3 col-sm-4 mt-sm-4" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-delay="500">
-                                        <img src="<?php bloginfo('template_directory') ?>/images/mitchell-lawler-tbaoryUol_E-unsplash.jpg" class="d-block w-100" alt="...">
-                                        <p class="deco-desc text-center center mt-2">Loreme ipqume sjmkmlssoioi</p>
+                                        <img src="<?php
+                                                    $image3 = $section_decoration['images']['image_3'];
+                                                    echo esc_url($image3['image']['url']);
+                                                    ?>
+                                        " class="d-block w-100" alt="...">
+                                        <p class="deco-desc text-center center mt-2"><?php echo $image3['description'] ?></p>
                                     </div>
                                     <div class="col-md-3 col-sm-4 mt-sm-4" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-delay="600">
-                                        <img src="<?php bloginfo('template_directory') ?>/images/mitchell-lawler-tbaoryUol_E-unsplash.jpg" class="d-block w-100" alt="...">
-                                        <p class="deco-desc text-center center mt-2">Loreme ipqume sjmkmlssoioi</p>
+                                        <img src="<?php
+                                                    $image4 = $section_decoration['images']['image_4'];
+                                                    echo esc_url($image4['image']['url']);
+                                                    ?>
+                                        " class="d-block w-100" alt="...">
+                                        <p class="deco-desc text-center center mt-2"><?php echo $image4['description'] ?></p>
                                     </div>
                                 </div>
                             </div>
                             <div class="carousel-item">
                                 <div class="row">
                                     <div class="col-md-3 col-sm-4 mt-sm-4 ">
-                                        <img src="<?php bloginfo('template_directory') ?>/images/frank-zhang-OjW_VqEP0So-unsplash.jpg" class="d-block w-100" alt="...">
-                                        <p class="deco-desc text-center mt-2">Loreme ipqume sjmkmlssoioi</p>
+                                        <img src="<?php
+                                                    $image5 = $section_decoration['images']['image_5'];
+                                                    echo esc_url($image5['image']['url']);
+                                                    ?>
+                                        " class="d-block w-100" alt="...">
+                                        <p class="deco-desc text-center mt-2"><?php echo $image5['description'] ?></p>
                                     </div>
                                     <div class="col-md-3 col-sm-4 mt-sm-4">
-                                        <img src="<?php bloginfo('template_directory') ?>/images/frank-zhang-OjW_VqEP0So-unsplash.jpg" class="d-block w-100" alt="...">
-                                        <p class="deco-desc text-center mt-2">Loreme ipqume sjmkmlssoioi</p>
+                                        <img src="<?php
+                                                    $image6 = $section_decoration['images']['image_6'];
+                                                    echo esc_url($image6['image']['url']);
+                                                    ?>
+                                        " class="d-block w-100" alt="...">
+                                        <p class="deco-desc text-center mt-2"><?php echo $image6['description'] ?></p>
                                     </div>
                                     <div class="col-md-3 col-sm-4 mt-sm-4">
-                                        <img src="<?php bloginfo('template_directory') ?>/images/frank-zhang-OjW_VqEP0So-unsplash.jpg" class="d-block w-100" alt="...">
-                                        <p class="deco-desc text-center mt-2">Loreme ipqume sjmkmlssoioi</p>
+                                        <img src="<?php
+                                                    $image7 = $section_decoration['images']['image_7'];
+                                                    echo esc_url($image7['image']['url']);
+                                                    ?>
+                                        " class="d-block w-100" alt="...">
+                                        <p class="deco-desc text-center mt-2"><?php echo $image7['description'] ?></p>
                                     </div>
                                     <div class="col-md-3 col-sm-4 mt-sm-4">
-                                        <img src="<?php bloginfo('template_directory') ?>/images/frank-zhang-OjW_VqEP0So-unsplash.jpg" class="d-block w-100" alt="...">
-                                        <p class="deco-desc text-center mt-2">Loreme ipqume sjmkmlssoioi</p>
+                                        <img src="<?php
+                                                    $image8 = $section_decoration['images']['image_8'];
+                                                    echo esc_url($image8['image']['url']);
+                                                    ?>
+                                        " alt="...">
+                                        <p class="deco-desc text-center mt-2"><?php echo $image8['description'] ?></p>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-
-
-
                     <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
                         <span class="sr-only">Previous</span>
                     </a>
@@ -164,9 +245,19 @@ get_header();
         <div class="container">
             <div class="row">
                 <div class="col-12 col-md-10 offset-md-1">
-                    <h3 class="text-center">Vidéo du mois</h3>
-                    <p class="text-center">Description</p>
-                    <iframe src="https://www.youtube.com/embed/HkuKHwetV6Q" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" class="video" allowfullscreen></iframe>
+                    <h3 class="text-center">
+                        <?php
+                        echo $section_video['titre'];
+                        ?>
+                    </h3>
+                    <p class="text-center">
+                        <?php
+                        echo $section_video['description'];
+                        ?>
+                    </p>
+                    <?php
+                    echo $section_video['lien'];
+                    ?>
                 </div>
             </div>
 
@@ -188,12 +279,15 @@ get_header();
                 <div class="col-md-10 offset-md-1">
                     <div class="row">
                         <div class="col-sm-4 col-lg-3" data-aos="fade-right">
-                            <img src="<?php bloginfo('template_directory') ?>/images/patricia-prudente-THcXu1LglG8-unsplash.jpg" class="bakery-image img-thumbnail" alt="Image devanture">
+                            <img src="<?php
+                                        echo esc_url($section_localisation['image']['url']);
+                                        ?>
+                            " class="bakery-image img-thumbnail" alt="Image devanture">
                         </div>
                         <div class="col-sm-7 text-right pt-sm-3 col-lg-8 ml-lg-3" data-aos="fade-up-left">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora sit eos expedita, at porro nostrum
-                            aliquid,
-                            quis vitae ipsa suscipit sequi reiciendis distinctio quasi cumque iusto magni nulla ut quaerat.
+                            <?php
+                            echo $section_localisation['localisation'];
+                            ?>
                         </div>
                     </div>
 
